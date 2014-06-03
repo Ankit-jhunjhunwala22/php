bff213aa52f297507150ba60b66904cd1099e736
jjthomson1 <ankit.jhunjhunwala22@gmail.com>
2014-06-03 15:10:51 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+<body >
+
+<form align="center" action="signup.php" method="POST" style="background:#A683BA;" name="signup">
+<h2>Sign up</h2>
+Username:<input size=30 type="text" name="username"/>
+<br><br><br>
+YourAGE:<input size=30 align="center" type="text" name="userage"/>
+<br><br><br>
+Youremail:<input size=30 type="text" name="email" />
+<br><br><br>
+password:<input size=30  type="password" name="password" />
+<br><br><br>
+<button type="submit" value="submit" size=20>submit</button>
+<br>
+<br>
+</form>
+<br><br>
+<form  action="action.php" method="POST" align="center" style="background:#9993BA;">
+<h2>LOGIN</h2>
+username:<input size=30 name="username" /><br><br>
+Password:<input size=30 type="password" name="password">
+<br>
+<button value="login" type="submit" size=20>login</button>
+</form>
+</body>
+</html>
